04d12f1498ffa73ff88a96f23c51c266b0659df4
srikanthpragada <srikanthpragada@gmail.com>
2025-01-08 17:54:00 +0530

    08-JAN

---
+++ structs/student.c
@@ -0,0 +1,25 @@
+
+# include <stdio.h>
+
+struct student
+{
+   int admno;
+   char name [20];
+   char email [30];
+   char course[10];
+};
+
+
+void main()
+{
+  struct student s1;
+
+     s1.admno = 1;
+     strcpy(s1.name, "Tom");
+     strcpy(s1.email, "tom@gmail.com");
+     strcpy(s1.course, "Java");
+
+
+     printf("%d %s %s %s", s1.admno, s1.name, s1.email, s1.course);
+
+}
